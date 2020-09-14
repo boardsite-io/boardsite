fe127aea187b8b14bbbef57e64fc2ac7648d5a3d
Andreas Koliopoulos <ga96leh@mytum.de>
2020-09-14 23:41:19 +0200

    Added Iconbuttons to Toolbar

--- src/component/navigation.js
+++ src/component/navigation.js
@@ -1,20 +1,39 @@
 import React from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
-import { Button, IconButton, Toolbar, Typography } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
+import { Grid, IconButton, Toolbar } from '@material-ui/core';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import BackupIcon from '@material-ui/icons/Backup';
+import ChatIcon from '@material-ui/icons/Chat';
+import AddIcon from '@material-ui/icons/Add';
+import HomeIcon from '@material-ui/icons/Home';
+
 
 function Navigation() {
     return (
         <AppBar position="static" color="primary">
             <Toolbar>
-                <IconButton edge="start" aria-label="menu">
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6">
-                    News
-                </Typography>
-                <Button color="inherit" style={{float: "right"}}>Login</Button>
+                <Grid
+                    justify="space-between"
+                    container
+                    spacing={0}
+                >
+                    <IconButton edge="start" aria-label="home">
+                        <HomeIcon />
+                    </IconButton>
+                    <IconButton edge="start" aria-label="add">
+                        <AddIcon />
+                    </IconButton>
+                    <IconButton edge="start" aria-label="account">
+                        <AccountBoxIcon />
+                    </IconButton>
+                    <IconButton edge="start" aria-label="backup">
+                        <BackupIcon />
+                    </IconButton>
+                    <IconButton edge="start" aria-label="chat">
+                        <ChatIcon />
+                    </IconButton>
+                </Grid>
             </Toolbar>
         </AppBar>
     );
