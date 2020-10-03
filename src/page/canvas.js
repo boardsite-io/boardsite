c7ebfabd4bf7d6bf783c7c0d00efcc5820937691
Andreas Koliopoulos <ga96leh@mytum.de>
2020-10-03 22:25:47 +0200

    added props to control the blockSize and blocksPerDim

--- src/page/canvas.js
+++ src/page/canvas.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 
 function Canvas(props) {
     const canvasRef = useRef(null);
@@ -6,22 +6,29 @@ function Canvas(props) {
     const wsRef = useRef(null);
 
     useEffect(() => {
+        function draw(location, index) {
+            let x = index % props.blocksPerDim;
+            let y = (index - index % props.blocksPerDim) / props.blocksPerDim;
+            let colorInt = location.color;
+            let color = "#" + colorInt.toString(16).padStart(6, "0");
+            //console.log("draw: x: " + x + " y: " + y + " c: " + color);
+            drawFillRect(x * props.blockSize, y * props.blockSize, props.blockSize, props.blockSize, color);
+        }
+
         const canvas = canvasRef.current
         const ctx = canvas.getContext('2d')
         ctx.clearRect(0, 0, window.innerHeight, window.innerWidth)
         props.locations.forEach((location, index) => {
             return draw(location, index);
         })
-    }, [draw, props.changeCounter, props.locations])
+    }, [props.changeCounter, props.locations, props.blockSize, props.blocksPerDim])
 
     // initialize the canvas context
     useEffect(() => {
         let isMounted = true;
 
         wsRef.current = new WebSocket("ws://heat.port0.org:8000/api/board");
-
         wsRef.current.onopen = () => { console.log('connected') }
-
         wsRef.current.onmessage = (data) => {
             if (isMounted) {
                 props.setChangeCounter((changeCounter) => changeCounter + 1);
@@ -31,7 +38,7 @@ function Canvas(props) {
                 let locations = props.locations;
                 message.forEach(location => {
                     //console.log(location);
-                    let index = 10 * location.y + location.x
+                    let index = props.blocksPerDim * location.y + location.x
                     locations[index] = { color: location.color };
                 })
                 props.setLocations(locations);
@@ -42,8 +49,8 @@ function Canvas(props) {
 
         // assign the width and height to canvas
         const canvasEle = canvasRef.current;
-        canvasEle.width = 500; //canvasEle.clientWidth;
-        canvasEle.height = 500; //canvasEle.clientHeight;
+        canvasEle.width = props.blockSize * props.blocksPerDim; //canvasEle.clientWidth;
+        canvasEle.height = props.blockSize * props.blocksPerDim; //canvasEle.clientHeight;
 
         // get context of the canvas
         ctxRef.current = canvasEle.getContext("2d");
@@ -65,14 +72,13 @@ function Canvas(props) {
     }
 
     function handleCanvasClick(e) {
-        let block_size = 50;
         let canvasElem = document.querySelector("canvas");
         let rect = canvasElem.getBoundingClientRect();
         let x = e.clientX - rect.left;
         let y = e.clientY - rect.top;
         const color = props.currColor;
-        x = (x - x % block_size) / block_size;
-        y = (y - y % block_size) / block_size;
+        x = (x - x % props.blockSize) / props.blockSize;
+        y = (y - y % props.blockSize) / props.blockSize;
         let colorInt = parseInt(color.substring(1), 16);
         // const newLocation = { x: x, y: y, color: colorInt };
         // props.setLocations(locations => [...locations, newLocation]);
@@ -83,16 +89,6 @@ function Canvas(props) {
         else {
             console.log("socket not open");
         }
-    }
-
-    function draw(location, index) {
-        let x = index % 10;
-        let y = (index - index % 10) / 10;
-        let colorInt = location.color;
-        let color = "#" + colorInt.toString(16).padStart(6, "0");
-        let block_size = 50;
-        //console.log("draw: x: " + x + " y: " + y + " c: " + color);
-        drawFillRect(x * block_size, y * block_size, block_size, block_size, color);
     }
 
     // draw rectangle with background
--- src/page/canvascontrol.js
+++ src/page/canvascontrol.js
@@ -7,13 +7,17 @@ import CanvasTools from './canvastools';
 function CanvasControl() {
     const [currColor, setCurrColor] = useState("#000000");
     const [locations, setLocations] = useState([]);
+    const [blockSize, setBlockSize] = useState(50);
+    const [blocksPerDim, setBlocksPerDim] = useState(10);
     const [changeCounter, setChangeCounter] = useState(0);
 
     return (
         <Container id="container" maxWidth="lg">
             <h1>Home</h1>
             <div className="homediv">
-                <Canvas changeCounter={changeCounter} setChangeCounter={setChangeCounter} currColor={currColor} locations={locations} setLocations={setLocations}/>
+                <Canvas changeCounter={changeCounter} setChangeCounter={setChangeCounter} currColor={currColor} 
+                        locations={locations} setLocations={setLocations} blocksPerDim={blocksPerDim} setBlocksPerDim={setBlocksPerDim}
+                        blockSize={blockSize} setBlockSize={setBlockSize}/>
                 <CanvasTools currColor={currColor} setCurrColor={setCurrColor} locations={locations} setLocations={setLocations}/>
             </div>
         </Container>
