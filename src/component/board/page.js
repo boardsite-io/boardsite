fd6608477a3ab48b14e85569ec6863a6e8c90bf4
Andreas Koliopoulos <ga96leh@mytum.de>
2021-01-31 18:06:21 +0100

    remove page selector validity check since its existence is guaranteed now

--- src/component/board/page.js
+++ src/component/board/page.js
@@ -4,14 +4,9 @@ import { StrokeShape } from "./stroke"
 
 export default function Page({ pageId }) {
     // console.log("Page Redraw")
-    const strokes = useSelector((state) => {
-        if (state.boardControl.present.pageCollection[pageId] !== undefined) {
-            return state.boardControl.present.pageCollection[pageId].strokes
-        }
-        // eslint-disable-next-line
-        console.log("pageCollection[pageId] is undefined")
-        return {}
-    })
+    const strokes = useSelector(
+        (state) => state.boardControl.present.pageCollection[pageId].strokes
+    )
 
     return (
         <>
