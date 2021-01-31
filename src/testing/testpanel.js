6b41e8a99d10deb9c546f9ecb79f3501c2906bd6
Andreas Koliopoulos <ga96leh@mytum.de>
2021-01-31 20:07:28 +0100

    testing interface updates

--- src/testing/testpanel.js
+++ src/testing/testpanel.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { useSelector } from "react-redux"
 import "./testpanel.css"
 
 import { Button } from "@material-ui/core"
@@ -7,42 +8,76 @@ import Tooltip from "@material-ui/core/Tooltip"
 import overload, { dispatchTest, dispatchTestSingle } from "./stresstest"
 
 export default function TestPanel() {
+    const isMouseDown = useSelector((state) => state.drawControl.isMouseDown)
+    const isDraggable = useSelector((state) => state.drawControl.isDraggable)
+    const isListening = useSelector((state) => state.drawControl.isListening)
     return (
-        <div className="testPanel">
-            <Tooltip
-                id="tooltip"
-                title="zoom out"
-                TransitionProps={{ timeout: 0 }}
-                placement="right">
-                <Button id="testPanelButtons">☯ TESTING ☯</Button>
-            </Tooltip>
-            <Tooltip
-                id="tooltip"
-                title="zoom out"
-                TransitionProps={{ timeout: 0 }}
-                placement="right">
-                <Button id="testPanelButtons" onClick={overload}>
-                    Stress Test
+        <>
+            <div className="testPanel">
+                <Tooltip
+                    id="tooltip"
+                    title="zoom out"
+                    TransitionProps={{ timeout: 0 }}
+                    placement="right">
+                    <Button id="testPanelButtons">☯ TESTING ☯</Button>
+                </Tooltip>
+                <Tooltip
+                    id="tooltip"
+                    title="zoom out"
+                    TransitionProps={{ timeout: 0 }}
+                    placement="right">
+                    <Button id="testPanelButtons" onClick={overload}>
+                        Stress Test
+                    </Button>
+                </Tooltip>
+                <Tooltip
+                    id="tooltip"
+                    title="zoom out"
+                    TransitionProps={{ timeout: 0 }}
+                    placement="right">
+                    <Button id="testPanelButtons" onClick={dispatchTest}>
+                        Update All Test
+                    </Button>
+                </Tooltip>
+                <Tooltip
+                    id="tooltip"
+                    title="zoom out"
+                    TransitionProps={{ timeout: 0 }}
+                    placement="right">
+                    <Button id="testPanelButtons" onClick={dispatchTestSingle}>
+                        Update One Test
+                    </Button>
+                </Tooltip>
+            </div>
+            <div className="testPanelStates">
+                <Button
+                    id={
+                        isMouseDown
+                            ? "testPanelButtonsActive"
+                            : "testPanelButtons"
+                    }
+                    variant="contained">
+                    isMouseDown
                 </Button>
-            </Tooltip>
-            <Tooltip
-                id="tooltip"
-                title="zoom out"
-                TransitionProps={{ timeout: 0 }}
-                placement="right">
-                <Button id="testPanelButtons" onClick={dispatchTest}>
-                    Update All Test
+                <Button
+                    id={
+                        isDraggable
+                            ? "testPanelButtonsActive"
+                            : "testPanelButtons"
+                    }
+                    variant="contained">
+                    ISDRAGGABLE
                 </Button>
-            </Tooltip>
-            <Tooltip
-                id="tooltip"
-                title="zoom out"
-                TransitionProps={{ timeout: 0 }}
-                placement="right">
-                <Button id="testPanelButtons" onClick={dispatchTestSingle}>
-                    Update One Test
+                <Button
+                    id={
+                        isListening
+                            ? "testPanelButtonsActive"
+                            : "testPanelButtons"
+                    }
+                    variant="contained">
+                    isListening
                 </Button>
-            </Tooltip>
-        </div>
+            </div>
+        </>
     )
 }
