31f42f77ceb981d581e97304097fe08852880006
Andreas Koliopoulos <ga96leh@mytum.de>
2021-02-17 00:10:11 +0100

    restructure session info map function to work with the given object structure

--- src/component/menu/sessioninfo.js
+++ src/component/menu/sessioninfo.js
@@ -2,25 +2,43 @@ import React from "react"
 import "../../css/sessioninfo.css"
 
 export default function SessionInfo() {
-    const info = [
-        { alias: "Gucciboi123", color: "#0f0", userid: "123uhih12" },
-        { alias: "hackermens1337", color: "#ff0", userid: "bquadratis24" },
-        { alias: "Troll22", color: "#074", userid: "xdddddd" },
-        { alias: "sheeshkebab", color: "#f0f", userid: "122222h12" },
-        { alias: "TheLegend27", color: "#fff", userid: "1567hzj12" },
-    ]
+    const userInfo = {
+        "123uhih12": {
+            alias: "Gucciboi123",
+            color: "#0f0",
+            userId: "123uhih12",
+        },
+        "1bquadratis24": {
+            alias: "hackermens1337",
+            color: "#ff0",
+            userId: "1bquadratis24",
+        },
+        "3xdddddd": { alias: "Troll22", color: "#074", userId: "3xdddddd" },
+        "122222h12": {
+            alias: "sheeshkebab",
+            color: "#f0f",
+            userId: "122222h12",
+        },
+        "1567hzj12": {
+            alias: "TheLegend27",
+            color: "#fff",
+            userId: "1567hzj12",
+        },
+    }
     return (
         <div className="session-info">
-            {/* <h1>Session Info:</h1> */}
-            {info.map((user) => (
-                <div key={user.userid} className="user-info">
-                    <div
-                        className="user-color"
-                        style={{ backgroundColor: user.color }}
-                    />
-                    <div className="user-alias">{user.alias}</div>
-                </div>
-            ))}
+            {Object.keys(userInfo).map((id) => {
+                const { alias, color, userId } = userInfo[id]
+                return (
+                    <div key={userId} className="user-info">
+                        <div
+                            className="user-color"
+                            style={{ backgroundColor: color }}
+                        />
+                        <div className="user-alias">{alias}</div>
+                    </div>
+                )
+            })}
         </div>
     )
 }
