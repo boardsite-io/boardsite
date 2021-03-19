d6d9a092776ac093019a527c62d7d9815cfe3a2e
Patrick Willner <patrickwillner@gmx.de>
2021-03-19 20:45:10 +0100

    Add electron build

---
+++ public/electron.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { app, BrowserWindow, Menu } = require("electron")
+const path = require("path")
+const isDev = require("electron-is-dev")
+
+function createWindow() {
+    // Create the browser window.
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+    })
+
+    win.loadURL(
+        isDev
+            ? "http://localhost:3000"
+            : `file://${path.join(__dirname, "../build/index.html")}`
+    )
+
+    Menu.setApplicationMenu(null)
+}
+
+app.on("ready", createWindow)
