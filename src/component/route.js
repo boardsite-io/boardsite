92a02d93c90f42da2eaed3dd30c016c0be1c3bb3
Patrick Willner <patrickwillner@gmx.de>
2021-03-20 19:16:52 +0100

    Select router based on whether electron is running

--- src/component/route.js
+++ src/component/route.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { HashRouter, Route } from "react-router-dom"
-
+import isElectron from "is-electron"
 import Whiteboard from "../view/whiteboard"
 
+const Router = isElectron() ? HashRouter : Route
+
 export default (
-    <HashRouter>
+    <Router>
         <Route exact path="/" component={Whiteboard} />
         <Route exact path="/b/:sid" component={Whiteboard} />
-    </HashRouter>
+    </Router>
 )
