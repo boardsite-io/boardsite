22be97a9b91a9803cf3f4a20903a64d5bcd56033
Patrick Willner <patrickwillner@gmx.de>
2021-03-21 18:04:05 +0100

    Remove settings dialog and add sidebar

--- src/component/menu/menucomponents/settings.js
+++ src/component/menu/menucomponents/settings.js
@@ -1,94 +1,109 @@
 import React, { useState } from "react"
 import { useSelector } from "react-redux"
 import { BsGear } from "react-icons/bs"
-import Button from "@material-ui/core/Button"
-import Dialog from "@material-ui/core/Dialog"
-import DialogActions from "@material-ui/core/DialogActions"
-import DialogContent from "@material-ui/core/DialogContent"
-// import DialogContentText from "@material-ui/core/DialogContentText"
-import DialogTitle from "@material-ui/core/DialogTitle"
-import FormLabel from "@material-ui/core/FormLabel"
-import FormControl from "@material-ui/core/FormControl"
-import FormGroup from "@material-ui/core/FormGroup"
-import FormControlLabel from "@material-ui/core/FormControlLabel"
-import Switch from "@material-ui/core/Switch"
+import { VscDebugDisconnect } from "react-icons/vsc"
+
 import {
-    TOGGLE_SHOULD_CENTER,
+    Divider,
+    Drawer,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    makeStyles,
+    Switch,
+    TextField,
+} from "@material-ui/core"
+import {
     TOGGLE_HIDE_NAVBAR,
+    TOGGLE_SHOULD_CENTER,
 } from "../../../redux/slice/viewcontrol"
 import store from "../../../redux/store"
 
+const useStyles = makeStyles({
+    paper: {
+        width: "250px",
+    },
+})
+
 export default function SettingsButton() {
-    const [open, setOpen] = useState(false)
+    const classes = useStyles()
+    const [isOpen, setOpen] = useState(false)
 
     const keepCentered = useSelector((state) => state.viewControl.keepCentered)
     const hideNavBar = useSelector((state) => state.viewControl.hideNavBar)
 
-    const handleClickOpen = () => {
-        setOpen(true)
-    }
-
-    const handleClose = () => {
-        setOpen(false)
+    const toggleDrawer = (open) => (event) => {
+        if (
+            event.type === "keydown" &&
+            (event.key === "Tab" || event.key === "Shift")
+        ) {
+            return
+        }
+        setOpen(open)
     }
 
     return (
         <>
-            <button type="button" id="icon-button" onClick={handleClickOpen}>
+            <button type="button" id="icon-button" onClick={toggleDrawer(true)}>
                 <BsGear id="icon" />
             </button>
-            <Dialog
-                maxWidth="xs"
-                fullWidth
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description">
-                <DialogTitle id="alert-dialog-title">Settings</DialogTitle>
-                <DialogContent>
-                    {/* <DialogContentText id="alert-dialog-description">
-                        Change global settings here.
-                    </DialogContentText> */}
-                    <FormControl component="fieldset">
-                        <FormLabel component="legend">
-                            General Settings
-                        </FormLabel>
-                        <FormGroup>
-                            <FormControlLabel
-                                control={
-                                    <Switch
-                                        checked={keepCentered}
-                                        onChange={() =>
-                                            store.dispatch(
-                                                TOGGLE_SHOULD_CENTER()
-                                            )
-                                        }
-                                        name="jason"
-                                    />
-                                }
-                                label="Keep view centered"
-                            />
-                            <FormControlLabel
-                                control={
-                                    <Switch
-                                        checked={hideNavBar}
-                                        onChange={() =>
-                                            store.dispatch(TOGGLE_HIDE_NAVBAR())
-                                        }
-                                        name="jason"
-                                    />
-                                }
-                                label="Hide navigation bar"
-                            />
-                        </FormGroup>
-                    </FormControl>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary">
-                        Close
-                    </Button>
-                </DialogActions>
-            </Dialog>
+
+            <Drawer
+                anchor="left"
+                open={isOpen}
+                onClose={toggleDrawer(false)}
+                classes={{ paper: classes.paper }}>
+                <List>
+                    <ListItem>
+                        <ListItemIcon>
+                            <BsGear />
+                        </ListItemIcon>
+                        <ListItemText primary="General Settings" />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="Keep view centered" />
+                        <Switch
+                            checked={keepCentered}
+                            onChange={() =>
+                                store.dispatch(TOGGLE_SHOULD_CENTER())
+                            }
+                            name="jason"
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="Hide navigation bar" />
+                        <Switch
+                            checked={hideNavBar}
+                            onChange={() =>
+                                store.dispatch(TOGGLE_HIDE_NAVBAR())
+                            }
+                            name="jason"
+                        />
+                    </ListItem>
+                </List>
+                <Divider />
+                <List>
+                    <ListItem>
+                        <ListItemIcon>
+                            <VscDebugDisconnect />
+                        </ListItemIcon>
+                        <ListItemText primary="Connection" />
+                    </ListItem>
+                    <ListItem>
+                        <TextField
+                            id="standard-basic"
+                            label="Custom API Server URL"
+                            defaultValue=""
+                            inputMode="url"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            helperText="e.g. https://api.boardsite.io"
+                        />
+                    </ListItem>
+                </List>
+            </Drawer>
         </>
     )
 }
