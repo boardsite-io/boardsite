25e4877289b7d2561d76e970653866d6b4c5dea5
Andreas Koliopoulos <andreas.koliopoulos@gmx.de>
2021-05-24 22:26:07 +0200

    new color picker tool

--- src/component/menu/menucomponents/colorpicker.tsx
+++ src/component/menu/menucomponents/colorpicker.tsx
@@ -1,92 +1,21 @@
 import React from "react"
-import { CustomPicker } from "react-color"
+import { HexColorPicker } from "react-colorful"
+import { useCustomSelector } from "../../../redux/hooks"
+import { SET_COLOR } from "../../../redux/slice/drawcontrol"
+import store from "../../../redux/store"
 
-// const { Alpha } = require("react-color/lib/components/common")
-import {
-    EditableInput,
-    Hue,
-    Saturation,
-} from "react-color/lib/components/common"
-// color, hex, hsl oldHue, onChange, rgb, source
+export default (): JSX.Element => {
+    const color = useCustomSelector(
+        (state) => state.drawControl.liveStroke.style.color
+    )
 
-interface ColorPickerProps {
-    color: any
-    hex?: any
-    hsl?: any
-    hsv?: any
-    oldHue?: any
-    onChange?: any
-    rgb?: any
-    source?: any
+    function handleChange(newColor: string | undefined) {
+        store.dispatch(SET_COLOR(newColor))
+    }
+
+    return (
+        <div className="color-picker">
+            <HexColorPicker color={color} onChange={handleChange} />
+        </div>
+    )
 }
-
-const ColorPicker: React.FC<ColorPickerProps> = ({
-    color,
-    hex,
-    hsl,
-    hsv,
-    oldHue,
-    onChange,
-    rgb,
-    source,
-}) => (
-    <div className="color-picker">
-        <div className="saturation">
-            <Saturation
-                color={color}
-                // hex={hex}
-                // hsl={hsl}
-                // hsv={hsv}
-                // oldHue={oldHue}
-                onChange={onChange}
-                // rgb={rgb}
-                // source={source}
-            />
-        </div>
-        <div className="hue">
-            <Hue
-                color={color}
-                // hex={hex}
-                // hsl={hsl}
-                // hsv={hsv}
-                // oldHue={oldHue}
-                onChange={onChange}
-                // rgb={rgb}
-                // source={source}
-                direction="horizontal"
-                pointer={() => <div className="hue-pointer" />} // leave css empty for no pointer
-            />
-        </div>
-        {/* <div className="alpha">
-                <Alpha
-                    color={color}
-                    hex={hex}
-                    hsl={hsl}
-                    hsv={hsv}
-                    oldHue={oldHue}
-                    onChange={onChange}
-                    rgb={rgb}
-                    source={source}
-                />
-            </div> */}
-        <div className="editableinput">
-            <EditableInput
-                value={color}
-                onChange={onChange}
-                style={{
-                    input: {
-                        border: "none",
-                        outline: "none",
-                        width: "190px",
-                        height: "20px",
-                        padding: "0px",
-                        paddingLeft: "5px",
-                        paddingRight: "5px",
-                    },
-                }}
-            />
-        </div>
-    </div>
-)
-
-export default CustomPicker(ColorPicker)
--- src/component/menu/menucomponents/stylepicker.tsx
+++ src/component/menu/menucomponents/stylepicker.tsx
@@ -1,22 +1,12 @@
 import React from "react"
 import ColorPicker from "./colorpicker"
 import WidthPicker from "./widthpicker"
-import { SET_COLOR } from "../../../redux/slice/drawcontrol"
-import store from "../../../redux/store"
 import "../../../css/stylepicker.css"
-import { useCustomSelector } from "../../../redux/hooks"
 
 export default function StylePicker() {
-    const colorSelector = useCustomSelector(
-        (state) => state.drawControl.liveStroke.style.color
-    )
-    function handleChange(e: any) {
-        store.dispatch(SET_COLOR(e.hex))
-    }
-
     return (
         <div className="style-picker">
-            <ColorPicker color={colorSelector} onChange={handleChange} />
+            <ColorPicker />
             <WidthPicker />
         </div>
     )
