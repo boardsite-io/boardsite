19a730f0643d128581aa3d2e720cb97c72da1a84
Patrick Willner <patrickwillner@gmx.de>
2021-05-24 15:43:44 +0200

    Add custom redux hooks

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import rootReducer from "./reducer"
 
-export default configureStore({
+const store = configureStore({
     reducer: rootReducer,
     middleware: [], // disable middleware
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export default store
