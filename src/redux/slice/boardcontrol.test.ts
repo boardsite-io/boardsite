17b0b4dd63eac221156da5f06fcc86aac4c2699e
Patrick Willner <50421879+heat1q@users.noreply.github.com>
2021-09-02 20:26:19 +0200

    Fix tests

--- src/redux/slice/boardcontrol.test.ts
+++ src/redux/slice/boardcontrol.test.ts
@@ -1,66 +1,24 @@
+import { BoardPage } from "drawing/page"
 import reducer from "./boardcontrol"
 import * as action from "./boardcontrol"
-import { Stroke } from "../../types"
+import { DocumentImage, Page, Stroke } from "../../types"
 import { BoardStroke } from "../../board/stroke/stroke"
+import { pageType } from "../../constants"
 
-const testState = {
-    pageRank: ["pid1", "pid2"],
-    pageCollection: {
-        pid1: {
-            strokes: {
-                strkid1: {
-                    id: "strkid1",
-                    pageId: "pid1",
-                },
-            },
-            meta: {
-                background: "checkered",
-            },
-        },
-        pid2: {
-            strokes: {
-                strkid2: {
-                    id: "strkid2",
-                    pageId: "pid2",
-                },
-            },
-            meta: {
-                background: "blank",
-            },
-        },
-    },
-}
+const page1 = new BoardPage(pageType.CHECKERED).setID("pid1")
+page1.strokes.strkid1 = new BoardStroke({
+    id: "strkid1",
+    pageId: "pid1",
+} as Stroke)
 
 describe("boardcontrol reducer", () => {
     it("should return the initial state", () => {
         expect(reducer(undefined, {} as any)).toEqual({
             pageRank: [],
             pageCollection: {},
-        })
-    })
-
-    it("should update the pages correctly", () => {
-        expect(
-            reducer(testState as any, action.SET_PAGERANK(["pid2", "pid3"]))
-        ).toEqual({
-            pageRank: ["pid2", "pid3"],
-            pageCollection: {
-                pid2: {
-                    strokes: {
-                        strkid2: {
-                            id: "strkid2",
-                            pageId: "pid2",
-                        },
-                    },
-                    meta: {
-                        background: "blank",
-                    },
-                },
-                pid3: {
-                    strokes: {},
-                    meta: {},
-                },
-            },
+            document: [] as DocumentImage[],
+            documentSrc: "",
+            pageBG: pageType.BLANK,
         })
     })
 
@@ -70,20 +28,11 @@ describe("boardcontrol reducer", () => {
                 {
                     pageRank: ["pid1"],
                     pageCollection: {
-                        pid1: {
-                            strokes: {
-                                strkid1: new BoardStroke({
-                                    id: "strkid1",
-                                    pageId: "pid1",
-                                    x: 0,
-                                    y: 0,
-                                } as Stroke),
-                            },
-                            meta: {
-                                background: "checkered",
-                            },
-                        },
+                        pid1: page1,
                     },
+                    document: [] as DocumentImage[],
+                    documentSrc: "",
+                    pageBG: pageType.BLANK,
                 },
                 action.UPDATE_STROKES([
                     {
