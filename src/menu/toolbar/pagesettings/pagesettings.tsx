38e627457391632254b39d2cdf5a4bb2051ae26e
Andreas Koliopoulos <andreas.koliopoulos@gmx.de>
2021-09-23 23:46:32 +0200

    Remove rest of material ui including the dependency
    Remove rest of material ui including the dependency
    Refactor pagesettings

--- src/menu/toolbar/pagesettings/pagesettings.tsx
+++ src/menu/toolbar/pagesettings/pagesettings.tsx
@@ -1,25 +1,28 @@
 import React, { useState } from "react"
 import { BsGear } from "react-icons/bs"
-import Button from "@material-ui/core/Button"
-import Dialog from "@material-ui/core/Dialog"
-import DialogActions from "@material-ui/core/DialogActions"
-import DialogContent from "@material-ui/core/DialogContent"
-// import DialogContentText from "@material-ui/core/DialogContentText"
 import { PageBackground } from "types"
-import DialogTitle from "@material-ui/core/DialogTitle"
 import { handlePageBackground } from "drawing/handlers"
-import { Grid } from "@material-ui/core"
-import { IconButton } from "components"
 import {
+    Button,
+    Dialog,
+    DialogContent,
+    DialogOptions,
+    DialogTitle,
+    IconButton,
+} from "components"
+import {
+    PageOptions,
     PagePreviewBlank,
     PagePreviewCheckered,
     PagePreviewRuled,
 } from "./pagesettings.styled"
 import { pageType } from "../../../constants"
 
-const ShapeTools: React.FC<{
+interface ShapeToolsProps {
     setOpenOther: React.Dispatch<React.SetStateAction<boolean>>
-}> = ({ setOpenOther }) => {
+}
+
+const ShapeTools: React.FC<ShapeToolsProps> = ({ setOpenOther }) => {
     const [open, setOpen] = useState(false)
 
     const handleClickOpen = () => {
@@ -41,49 +44,27 @@ const ShapeTools: React.FC<{
             <IconButton onClick={handleClickOpen}>
                 <BsGear id="icon" />
             </IconButton>
-            <Dialog
-                maxWidth="xs"
-                fullWidth
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description">
-                <DialogTitle id="alert-dialog-title">
-                    Select page background style
-                </DialogTitle>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Select page background style</DialogTitle>
                 <DialogContent>
-                    <Grid
-                        item
-                        container
-                        spacing={2}
-                        direction="row"
-                        justifyContent="center"
-                        alignItems="center">
-                        <Grid item>
-                            <PagePreviewBlank
-                                type="button"
-                                onClick={handleClick(pageType.BLANK)}
-                            />
-                        </Grid>
-                        <Grid item>
-                            <PagePreviewCheckered
-                                type="button"
-                                onClick={handleClick(pageType.CHECKERED)}
-                            />
-                        </Grid>
-                        <Grid item>
-                            <PagePreviewRuled
-                                type="button"
-                                onClick={handleClick(pageType.RULED)}
-                            />
-                        </Grid>
-                    </Grid>
+                    <PageOptions>
+                        <PagePreviewBlank
+                            type="button"
+                            onClick={handleClick(pageType.BLANK)}
+                        />
+                        <PagePreviewCheckered
+                            type="button"
+                            onClick={handleClick(pageType.CHECKERED)}
+                        />
+                        <PagePreviewRuled
+                            type="button"
+                            onClick={handleClick(pageType.RULED)}
+                        />
+                    </PageOptions>
                 </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary">
-                        Close
-                    </Button>
-                </DialogActions>
+                <DialogOptions>
+                    <Button onClick={handleClose}>Close</Button>
+                </DialogOptions>
             </Dialog>
         </>
     )
