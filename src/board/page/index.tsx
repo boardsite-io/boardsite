cc124c860c1dacb2d9ad2aeb4dcaba6b2cfeade8
Andreas Koliopoulos <andreas.koliopoulos@gmx.de>
2021-10-05 19:58:55 +0200

    Rename page content to strokes

--- src/board/page/index.tsx
+++ src/board/page/index.tsx
@@ -1,13 +1,13 @@
 import React, { FC } from "react"
 import PageListener from "./listener"
-import PageContent from "./content"
+import Strokes from "./strokes"
 import PageBackground from "./background"
 import { PageProps } from "./types"
 
 const Page: FC<PageProps> = (props) => (
     <>
         <PageBackground {...props} />
-        <PageContent {...props} />
+        <Strokes {...props} />
         <PageListener {...props} />
     </>
 )
