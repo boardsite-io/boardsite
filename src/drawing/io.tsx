b9d3e269b07ab2c85d3927c4a9c4504cb68ba1d9
Andreas Koliopoulos <andreas.koliopoulos@gmx.de>
2021-10-21 20:19:26 +0200

    Adjust PDF export to individual page sizes

--- src/drawing/io.tsx
+++ src/drawing/io.tsx
@@ -6,7 +6,7 @@ import { Provider } from "react-redux"
 import { Layer, Stage, Rect } from "react-konva"
 import * as types from "konva/types/Layer"
 import { StrokeShape } from "board/stroke/shape"
-import { CANVAS_HEIGHT, CANVAS_WIDTH, pageType } from "consts"
+import { pageType } from "consts"
 import store from "../redux/store"
 import { pageBackground } from "./page"
 
@@ -14,14 +14,15 @@ export async function pagesToDataURL(
     pixelRatio: number,
     drawBackground?: boolean
 ): Promise<string[]> {
-    const pages = store.getState().board.pageRank
+    const { pageRank } = store.getState().board
 
-    const imgs = pages.map(async (pageId: string) => {
+    const imgs = pageRank.map(async (pageId: string) => {
         const strokeIds = Object.keys(
             store.getState().board.pageCollection[pageId].strokes
         )
-        const { style } =
-            store.getState().board.pageCollection[pageId]?.meta?.background
+        const { meta } = store.getState().board.pageCollection[pageId]
+        const { style } = meta.background
+        const { height, width } = meta
 
         const tmp = document.createElement("div")
 
@@ -29,12 +30,12 @@ export async function pagesToDataURL(
         const LayerTest = () => {
             ref = React.useRef<types.Layer>(null)
             return (
-                <Stage height={CANVAS_HEIGHT} width={CANVAS_WIDTH}>
+                <Stage height={height} width={width}>
                     <Provider store={store}>
                         <Layer ref={ref}>
                             <Rect
-                                height={CANVAS_HEIGHT}
-                                width={CANVAS_WIDTH}
+                                height={height}
+                                width={width}
                                 sceneFunc={
                                     drawBackground
                                         ? pageBackground[style]
@@ -93,17 +94,18 @@ export async function toPDF(
     const { pageRank, pageCollection } = store.getState().board
 
     pageImages.forEach((pageData, i) => {
-        const { style, documentPageNum } =
-            pageCollection[pageRank[i]].meta.background
+        const { width, height, background } = pageCollection[pageRank[i]].meta
+        const { style, documentPageNum } = background
         const page =
             style === pageType.DOC
                 ? pdf.addPage(pdfPages[documentPageNum])
                 : pdf.addPage()
+        page.setSize(width, height)
         page.drawImage(pageData, {
             x: 0,
             y: 0,
-            width: page.getSize().width,
-            height: page.getSize().height,
+            width,
+            height,
         })
     })
 
