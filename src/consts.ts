299cffe2145849d26d3d7a3b2fb462e0d689837c
Andreas Koliopoulos <andreas.koliopoulos@gmx.de>
2021-10-29 19:29:11 +0200

    Fix resizing behaviour (#81)
    Fix resizing behaviour for:
    - inital mount
    - rotating phone
    -> debounce the resizing for the resize listener and add const

--- src/consts.ts
+++ src/consts.ts
@@ -8,6 +8,7 @@ export default function isDev(): boolean {
     return !process.env.NODE_ENV || process.env.NODE_ENV === "development"
 }
 
+export const RESIZE_DEBOUNCE = 250 // debounce time in ms
 export const PIXEL_RATIO = 4
 export const STROKE_WIDTH_PRESETS = [0.5, 1, 2, 3, 4, 5, 7, 10, 14, 20]
 export const DEFAULT_PAGE_GAP = 20
