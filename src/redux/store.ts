deb5275bc3da0519216a6927edb5925394a57815
Andreas Koliopoulos <andreas.koliopoulos@gmx.de>
2021-11-01 23:25:24 +0100

    Update stage dimensions on indexedDB load
    Update stage dimensions on indexedDB load instead of App mount

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -35,6 +35,10 @@ const store = configureStore({
         type: "board/LOAD_BOARD_STATE",
         payload: state.board,
     } as AnyAction)
+    store.dispatch({
+        type: "board/CENTER_VIEW",
+        payload: state.board,
+    } as AnyAction)
 })()
 
 export type AppDispatch = typeof store.dispatch
