ab07b1d335350115a4ec63de092b649121a5d27c
Patrick Willner <50421879+heat1q@users.noreply.github.com>
2021-11-25 09:16:38 +0100

    Remove pointsSegment in livestroke (#114)
    - the segmentation is done in the livestroke rendering
    - declare register function as static
    - apply RDP algorithm when processing points in order to remove ~25% of redundant points

--- src/board/stroke/livestroke.tsx
+++ src/board/stroke/livestroke.tsx
@@ -1,5 +1,6 @@
+import { LIVESTROKE_SEGMENT_SIZE } from "consts"
 import { LiveStroke } from "drawing/stroke/types"
-import React, { memo } from "react"
+import React, { memo, useCallback } from "react"
 import { StrokeShape } from "./shape"
 
 interface LiveStrokeShapeProps {
@@ -9,18 +10,37 @@ interface LiveStrokeShapeProps {
 
 export const LiveStrokeShape = memo<LiveStrokeShapeProps>(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    ({ liveStroke, liveStrokeTrigger }) => (
-        <>
-            {liveStroke().pointsSegments.map((subPts: number[], i: number) => {
-                const strokeSegment = {
-                    ...liveStroke(),
-                    points: subPts.slice(),
-                } as LiveStroke
-                // we can use the array index as key here
-                // since the array's order is not changed
-                // eslint-disable-next-line react/no-array-index-key
-                return <StrokeShape key={i} stroke={strokeSegment} />
-            })}
-        </>
-    )
+    ({ liveStroke, liveStrokeTrigger }) => {
+        const getSegments = useCallback(
+            () =>
+                new Array<number[]>(
+                    Math.ceil(
+                        liveStroke().points.length / LIVESTROKE_SEGMENT_SIZE
+                    )
+                )
+                    .fill([])
+                    .map((seg, i) =>
+                        liveStroke().points.slice(
+                            LIVESTROKE_SEGMENT_SIZE * i,
+                            LIVESTROKE_SEGMENT_SIZE * (i + 1) + 2
+                        )
+                    ),
+            []
+        )
+
+        return (
+            <>
+                {getSegments().map((subPts: number[], i: number) => {
+                    const strokeSegment = {
+                        ...liveStroke(),
+                        points: subPts.slice(),
+                    } as LiveStroke
+                    // we can use the array index as key here
+                    // since the array's order is not changed
+                    // eslint-disable-next-line react/no-array-index-key
+                    return <StrokeShape key={i} stroke={strokeSegment} />
+                })}
+            </>
+        )
+    }
 )
--- src/drawing/stroke/simplify.ts
+++ src/drawing/stroke/simplify.ts
@@ -1,4 +1,5 @@
 import { getStrokePoints, StrokeOptions } from "perfect-freehand"
+import { Point } from "./types"
 
 export const perfectDrawing = (points: number[]): number[] => {
     const formattedPoints = new Array(points.length / 2)
@@ -7,13 +8,13 @@ export const perfectDrawing = (points: number[]): number[] => {
 
     const options: StrokeOptions = {
         // The base size (diameter) of the stroke.
-        size: undefined, // 1 + styles.strokeWidth * 1.5,
+        size: 0, // 1 + styles.strokeWidth * 1.5,
         // The effect of pressure on the stroke's size.
-        thinning: 0.65,
+        thinning: 0,
         // How much to soften the stroke's edges.
-        smoothing: 0.65,
+        smoothing: 0,
         // How much to streamline the stroke.
-        streamline: 0.65,
+        streamline: 0.25,
         // Whether to simulate pressure based on velocity.
         simulatePressure: false,
         // An easing function to apply to each point's pressure.
@@ -31,10 +32,95 @@ export const perfectDrawing = (points: number[]): number[] => {
         //     easing: (t) => t,
         // },
         // Whether the stroke is complete.
-        last: true,
+        last: false,
     }
 
     return getStrokePoints(formattedPoints, options)
         .map((strokePoint) => strokePoint.point)
         .flat()
+}
+
+/**
+ * Find the perpendicular distance between a point and a reference line.
+ * @param point
+ * @param line
+ * @returns Perpendicular distance
+ */
+export function perpendicularDistance(
+    point: Point,
+    line: [Point, Point]
+): number {
+    const slope = (line[1].y - line[0].y) / (line[1].x - line[0].x)
+    const intercept = line[0].y - slope * line[0].x
+    return (
+        Math.abs(slope * point.x - point.y + intercept) /
+        Math.sqrt(slope ** 2 + 1)
+    )
+}
+
+/**
+ * Ramer–Douglas–Peucker algorithm, an algorithm that decimates
+ * a curve composed of line segments to a similar curve
+ * with fewer points. Parameter section will guarantee that the
+ * algorithm splits the curve in at least 2^(sections) sections.
+ * @param {[number]} points
+ * @param {number} epsilon
+ * @param {number} sections
+ */
+// eslint-disable-next-line import/prefer-default-export
+export function simplifyRDP(
+    points: number[],
+    epsilon: number,
+    sections: number
+): number[] {
+    if (points.length <= 4) {
+        return points
+    }
+
+    let maxIndex = 0
+    let maxDistance = 0
+    let dist
+    let filteredPoints
+
+    if (sections > 0) {
+        maxIndex = points.length / 2 + ((points.length / 2) % 2)
+        maxDistance = epsilon
+        sections -= 1
+    }
+
+    // find the point with the maximum distance
+    for (let i = 2; i < points.length - 2; i += 2) {
+        dist = perpendicularDistance({ x: points[i], y: points[i + 1] }, [
+            { x: points[0], y: points[1] },
+            { x: points[points.length - 2], y: points[points.length - 1] },
+        ])
+        if (dist > maxDistance) {
+            maxIndex = i
+            maxDistance = dist
+        }
+    }
+    // if max distance is greater than epsilon, recursively simplify
+    if (maxDistance >= epsilon) {
+        const left = simplifyRDP(points.slice(0, maxIndex), epsilon, sections)
+        if (maxIndex - 2 === 0) {
+            const right = simplifyRDP(points.slice(maxIndex), epsilon, sections)
+            filteredPoints = left.concat(right)
+        } else {
+            const right = simplifyRDP(
+                points.slice(maxIndex - 2),
+                epsilon,
+                sections
+            )
+            filteredPoints = left.concat(right.slice(2))
+        }
+    } else {
+        filteredPoints = [
+            points[0],
+            points[1],
+            points[points.length - 2],
+            points[points.length - 1],
+        ]
+    }
+
+    return filteredPoints
 }
