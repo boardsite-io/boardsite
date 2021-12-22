83f399a5251f99147ca880508ddc935d0c8b3ddb
Andreas Koliopoulos <andreas.koliopoulos@gmx.de>
2021-12-22 18:35:19 +0100

    Redirect invalid routes to main route /

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,11 +1,12 @@
 import React from "react"
-import { Route, Routes as RouterRoutes } from "react-router-dom"
+import { Route, Routes as RouterRoutes, Navigate } from "react-router-dom"
 import Whiteboard from "view/whiteboard"
 
 const Routes = (): JSX.Element => (
     <RouterRoutes>
         <Route path="/" element={<Whiteboard />} />
         <Route path="/b/:sid" element={<Whiteboard />} />
+        <Route path="*" element={<Navigate to="/" />} />
     </RouterRoutes>
 )
 
