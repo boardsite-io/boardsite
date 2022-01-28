5fc1bbfec1149ca2dc371527d0c89bd88340eef8
Andreas Koliopoulos <andreas.koliopoulos@gmx.de>
2022-01-28 19:12:18 +0100

    Fix joining session with link (#189)
    Fix joining session with link and refactor session menu

--- src/View/SessionMenu/index.tsx
+++ src/View/SessionMenu/index.tsx
@@ -1,28 +1,15 @@
-import React, { useEffect } from "react"
+import React from "react"
 import store from "redux/store"
 import { Dialog } from "components"
 import { useCustomSelector } from "hooks"
-import { NavigateFunction, useNavigate, useParams } from "react-router-dom"
-import { currentSession, isConnected } from "api/session"
+import { useParams } from "react-router-dom"
+import { isConnected } from "api/session"
 import { SET_SESSION_DIALOG } from "redux/session/session"
 import { DialogState } from "redux/session/session.types"
 import ManageOnlineSession from "./ManageOnlineSession"
 import CreateOnlineSession from "./CreateOnlineSession"
 import InitialSelection from "./InitialSelection"
 import JoinOnly from "./JoinOnly"
-
-const checkSessionStatus = async (sid: string, navigate: NavigateFunction) => {
-    try {
-        await currentSession().setID(sid).ping()
-
-        // Session exists
-        store.dispatch(SET_SESSION_DIALOG(DialogState.JoinOnly))
-    } catch (error) {
-        store.dispatch(SET_SESSION_DIALOG(DialogState.CreateOnlineSession))
-        // Session doesn't exist
-        navigate("/")
-    }
-}
 
 const handleClose = () => {
     store.dispatch(SET_SESSION_DIALOG(DialogState.Closed))
@@ -38,19 +25,14 @@ const contents = {
 }
 
 const Session: React.FC = () => {
-    const navigate = useNavigate()
     const dialogState = useCustomSelector((state) => state.session.dialogState)
     const { sid } = useParams()
-
-    useEffect(() => {
-        if (sid && !isConnected()) {
-            checkSessionStatus(sid, navigate)
-        }
-    }, [sid])
+    const contentType =
+        sid && !isConnected() ? DialogState.JoinOnly : dialogState
 
     return (
         <Dialog open={dialogState !== DialogState.Closed} onClose={handleClose}>
-            {contents[dialogState]}
+            {contents[contentType]}
         </Dialog>
     )
 }
