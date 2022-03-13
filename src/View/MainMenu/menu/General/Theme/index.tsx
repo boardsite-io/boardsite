879d9a52362b9933c753ae3cd48b47b8d823caa3
Andreas Koliopoulos <andreas.koliopoulos@gmx.de>
2022-03-13 21:26:53 +0100

    Show but disable sub themes for non subs
    Show but disable sub themes for non subs

--- src/View/MainMenu/menu/General/Theme/index.tsx
+++ src/View/MainMenu/menu/General/Theme/index.tsx
@@ -9,7 +9,7 @@ import MenuItem from "../../../MenuItem"
 
 const ThemeMenu = () => {
     const { theme: activeTheme } = useTheme("theme")
-    const { isAuthorized } = useOnline("session")
+    const isAuthorized = useOnline("session").isAuthorized()
 
     return (
         <SubMenuWrap>
@@ -23,60 +23,54 @@ const ThemeMenu = () => {
                 onClick={() => theme.setTheme(Theme.Dark)}
                 icon={activeTheme === Theme.Dark && <TickIcon />}
             />
-            {isAuthorized() && (
-                <>
-                    <MenuItem
-                        text={
-                            <FormattedMessage id="Menu.General.Theme.Apple" />
-                        }
-                        onClick={() => theme.setTheme(Theme.Apple)}
-                        icon={activeTheme === Theme.Apple && <TickIcon />}
-                    />
-                    <MenuItem
-                        text={
-                            <FormattedMessage id="Menu.General.Theme.Candy" />
-                        }
-                        onClick={() => theme.setTheme(Theme.Candy)}
-                        icon={activeTheme === Theme.Candy && <TickIcon />}
-                    />
-                    <MenuItem
-                        text={
-                            <FormattedMessage id="Menu.General.Theme.Ocean" />
-                        }
-                        onClick={() => theme.setTheme(Theme.Ocean)}
-                        icon={activeTheme === Theme.Ocean && <TickIcon />}
-                    />
-                    <MenuItem
-                        text={
-                            <FormattedMessage id="Menu.General.Theme.Orange" />
-                        }
-                        onClick={() => theme.setTheme(Theme.Orange)}
-                        icon={activeTheme === Theme.Orange && <TickIcon />}
-                    />
-                    <MenuItem
-                        text={
-                            <FormattedMessage id="Menu.General.Theme.Purple" />
-                        }
-                        onClick={() => theme.setTheme(Theme.Purple)}
-                        icon={activeTheme === Theme.Purple && <TickIcon />}
-                    />
-                    <MenuItem
-                        text={<FormattedMessage id="Menu.General.Theme.Soil" />}
-                        onClick={() => theme.setTheme(Theme.Soil)}
-                        icon={activeTheme === Theme.Soil && <TickIcon />}
-                    />
-                    <MenuItem
-                        text={<FormattedMessage id="Menu.General.Theme.Sun" />}
-                        onClick={() => theme.setTheme(Theme.Sun)}
-                        icon={activeTheme === Theme.Sun && <TickIcon />}
-                    />
-                    <MenuItem
-                        text={<FormattedMessage id="Menu.General.Theme.Teal" />}
-                        onClick={() => theme.setTheme(Theme.Teal)}
-                        icon={activeTheme === Theme.Teal && <TickIcon />}
-                    />
-                </>
-            )}
+            <MenuItem
+                disabled={!isAuthorized}
+                text={<FormattedMessage id="Menu.General.Theme.Apple" />}
+                onClick={() => theme.setTheme(Theme.Apple)}
+                icon={activeTheme === Theme.Apple && <TickIcon />}
+            />
+            <MenuItem
+                disabled={!isAuthorized}
+                text={<FormattedMessage id="Menu.General.Theme.Candy" />}
+                onClick={() => theme.setTheme(Theme.Candy)}
+                icon={activeTheme === Theme.Candy && <TickIcon />}
+            />
+            <MenuItem
+                disabled={!isAuthorized}
+                text={<FormattedMessage id="Menu.General.Theme.Ocean" />}
+                onClick={() => theme.setTheme(Theme.Ocean)}
+                icon={activeTheme === Theme.Ocean && <TickIcon />}
+            />
+            <MenuItem
+                disabled={!isAuthorized}
+                text={<FormattedMessage id="Menu.General.Theme.Orange" />}
+                onClick={() => theme.setTheme(Theme.Orange)}
+                icon={activeTheme === Theme.Orange && <TickIcon />}
+            />
+            <MenuItem
+                disabled={!isAuthorized}
+                text={<FormattedMessage id="Menu.General.Theme.Purple" />}
+                onClick={() => theme.setTheme(Theme.Purple)}
+                icon={activeTheme === Theme.Purple && <TickIcon />}
+            />
+            <MenuItem
+                disabled={!isAuthorized}
+                text={<FormattedMessage id="Menu.General.Theme.Soil" />}
+                onClick={() => theme.setTheme(Theme.Soil)}
+                icon={activeTheme === Theme.Soil && <TickIcon />}
+            />
+            <MenuItem
+                disabled={!isAuthorized}
+                text={<FormattedMessage id="Menu.General.Theme.Sun" />}
+                onClick={() => theme.setTheme(Theme.Sun)}
+                icon={activeTheme === Theme.Sun && <TickIcon />}
+            />
+            <MenuItem
+                disabled={!isAuthorized}
+                text={<FormattedMessage id="Menu.General.Theme.Teal" />}
+                onClick={() => theme.setTheme(Theme.Teal)}
+                icon={activeTheme === Theme.Teal && <TickIcon />}
+            />
         </SubMenuWrap>
     )
 }
