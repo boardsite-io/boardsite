b5a50a5d714ecc0bbb1fae3af512adaf24e05c86
Andreas Koliopoulos <andreas.koliopoulos@gmx.de>
2022-03-20 22:35:09 +0100

    Util upgrades
    Functional upgrade
    
    Co-Authored-By: Patrick Willner <50421879+heat1q@users.noreply.github.com>

--- src/util/color/index.tsx
+++ src/util/color/index.tsx
@@ -1,14 +1,11 @@
 import { StrokeStyle } from "View/Board/RenderNG/index.types"
 
 export const getRandomColor = () => {
-    const letters = "0123456789ABCDEF"
-    let color = "#"
-
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)]
-    }
-
-    return color
+    const color = new Array(6)
+        .fill(0)
+        .map(() => Math.floor(Math.random() * 16).toString(16))
+        .join("")
+    return "#".concat(color)
 }
 
 export const strokeStyleToRGBA = (style: StrokeStyle): string => {
