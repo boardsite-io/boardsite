4ca3090c059dc4a750f2f6f1bffd2436e566047e
Andreas Koliopoulos <andreas.koliopoulos@gmx.de>
2022-09-17 17:23:11 +0200

    Cache window dimensions for performance
    Cache window dimensions to avoid the synchronous calls to the browser window which have a negative impact on performance

--- src/state/view/util/bounds.ts
+++ src/state/view/util/bounds.ts
@@ -3,39 +3,51 @@ import {
     SCROLL_LIMIT_HORIZONTAL,
     SCROLL_LIMIT_LAST_PAGE,
 } from "consts"
-import { ViewTransform } from "state/view/state/index.types"
+import { ViewState, ViewTransform } from "state/view/state/index.types"
 
-interface GetHorizontalBoundsProps {
+type GetHorizontalBoundsProps = {
     newTransform: ViewTransform
     pageWidth: number
     keepCentered: boolean
-}
+} & Pick<ViewState, "innerWidth">
 
 export const getHorizontalBounds = ({
     newTransform,
     pageWidth,
     keepCentered,
+    innerWidth,
 }: GetHorizontalBoundsProps) => {
     const limit = keepCentered ? 1 : SCROLL_LIMIT_HORIZONTAL
-    const offset =
-        (window.innerWidth * limit) / newTransform.scale - pageWidth / 2
+    const offset = (innerWidth * limit) / newTransform.scale - pageWidth / 2
     return {
         leftBound: offset,
-        rightBound: window.innerWidth / newTransform.scale - offset,
+        rightBound: innerWidth / newTransform.scale - offset,
     }
 }
 
-export const getUpperBound = (newTransform: ViewTransform): number => {
-    return (window.innerHeight * SCROLL_LIMIT_FIRST_PAGE) / newTransform.scale
+type GetUpperBoundProps = {
+    newTransform: ViewTransform
+} & Pick<ViewState, "innerHeight">
+
+export const getUpperBound = ({
+    newTransform,
+    innerHeight,
+}: GetUpperBoundProps): number => {
+    return (innerHeight * SCROLL_LIMIT_FIRST_PAGE) / newTransform.scale
 }
 
-export const getLowerBound = (
-    newTransform: ViewTransform,
+type GetLowerBoundProps = {
+    newTransform: ViewTransform
     pageHeight: number
-): number => {
+} & Pick<ViewState, "innerHeight">
+
+export const getLowerBound = ({
+    newTransform,
+    pageHeight,
+    innerHeight,
+}: GetLowerBoundProps): number => {
     return (
-        (window.innerHeight * SCROLL_LIMIT_LAST_PAGE) / newTransform.scale -
-        pageHeight
+        (innerHeight * SCROLL_LIMIT_LAST_PAGE) / newTransform.scale - pageHeight
     )
 }
 
